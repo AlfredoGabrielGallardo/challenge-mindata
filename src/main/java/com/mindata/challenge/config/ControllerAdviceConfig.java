b5a5850f8b95f2612360d1e222ccef0e0f4e1507
Fredi <agallardo@cash-online.com.ar>
2023-03-10 19:50:35 -0300

    Adding configurations

--- src/main/java/com/mindata/challenge/config/ControllerAdviceConfig.java
+++ src/main/java/com/mindata/challenge/config/ControllerAdviceConfig.java
@@ -1,4 +1,46 @@
 package com.mindata.challenge.config;
 
-public class ControllerAdviceConfig {
+import com.mindata.challenge.model.exception.DuplicatedHeroException;
+import com.mindata.challenge.model.exception.HeroNotFoundException;
+import com.mindata.challenge.model.exception.InvalidHeroIdException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class ControllerAdviceConfig{
+
+    @ExceptionHandler(value = DuplicatedHeroException.class)
+    public ResponseEntity<Map<String, String>> handleDuplicatedException(DuplicatedHeroException e) {
+        log.error(String.format("[DUPLICATE HERO]: %s", e.getLocalizedMessage()));
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(Map.of("[ERROR]", e.getMessage()));
+    }
+
+    @ExceptionHandler(value = InvalidHeroIdException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidHeroIdException(InvalidHeroIdException e) {
+        log.error(String.format("[INVALID HERO ID]: %s", e.getLocalizedMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("[ERROR]", e.getMessage()));
+    }
+
+    @ExceptionHandler(value = HeroNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleHeroNotFoundException(HeroNotFoundException e) {
+        log.error(String.format("[HERO NOT FOUND]: %s", e.getLocalizedMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("[ERROR]", e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        log.error(String.format("[WRONG VALIDATION]: %s", e.getLocalizedMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("[ERROR]", "Validation failed"));
+    }
 }
